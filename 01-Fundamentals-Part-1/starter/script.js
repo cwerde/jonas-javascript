1baba6cb291d6f4cdf3d1150a21fde9a6d5fd68c
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-01 00:34:21 +0300

    Lecture 10 Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,49 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
-40 + 8 + 23 - 10;
+// 40 + 8 + 23 - 10
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+// let first = "Jonas";
+// let firstNamePerson;
+// let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable names cannot start with numbers.
+// let 3years = 3;
+
+// Variable names can only contain numbers, letters, underscores or the dollar sign.
+// Wrong // let jonas&matilda = "JM";
+// Correct // let jonas_matilda = "JM";
+
+// We cannot name a variable with a reserved JavaScript keyword.
+// Wrong // let new = 27;
+// Wrong // let function = 27;
+// We can solve this problem easy with putting a legal character before this keywords.
+// Correct // let _function = 27;
+// Correct // let $function = 27;
+
+// "name" is also a keyword but we can still use it. But in some cases it causes some problems. So always prefer to use "firstName" or with a legal symbol.
+// let name = "Jonas";
+
+// Variable names shouldn't start with uppercase letters because of the variables in Object-Oriented programming start with uppercase letters.
+// let Person = "Jonas";
+
+// We write all letters in uppercase if we want to make a variable be constant.
+// let PI = 3.1415;
+
+// Our variables should be easy to understand what value they are holding just by reading the name of the variable.
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
