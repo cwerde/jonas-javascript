cd4ba807404d328abbdbcf8f1a10f47d573d3976
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-01 23:48:49 +0300

    Lecture 14 Update

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,5 @@
 // LECTURE: Values and Variables
-
+/*
 const country = "Turkey";
 const continent = "Europe";
 let population = 84340000;
@@ -7,9 +7,10 @@ let population = 84340000;
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
 
 // LECTURE: Data Types
-
+/*
 const isIsland = false;
 let language;
 
@@ -18,7 +19,10 @@ console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
 console.log(typeof language);
+*/
 
 // LECTURE: let, const and var
+/*
 language = "Turkish";
 population = 90000000;
+*/
