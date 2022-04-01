297d4f29dc41432e39e04bb70fbfddb1e6cfcfa4
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-01 23:48:27 +0300

    Lecture 14 Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -50,6 +50,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 30;
 age = 31;
 
@@ -62,3 +63,34 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
+*/
+
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
