46d44881155668bb58f14acc07eb8b771ff50775
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-04 00:32:36 +0300

    Lecture 15 Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -65,6 +65,7 @@ lastName = "Schmedtmann";
 console.log(lastName);
 */
 
+/*
 // Math Operators
 const now = 2037;
 const ageJonas = now - 1991;
@@ -94,3 +95,19 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
