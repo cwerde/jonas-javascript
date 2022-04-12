5774ac20f35c9b1e461c5aa2c76286801d2c27b3
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-13 00:12:28 +0300

    Lecture 33 Update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,32 @@ if (hasDriversLicense) console.log("I can drive :D");
 // const interface = "Audio";
 // const private = 534;
 */
+
+// Lecture 33: Functions
+
+function logger() {
+  console.log("My name is Jonas.");
+}
+
+// Calling / Running / Invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} and ${oranges} oranges.`;
+  return juice;
+}
+
+// fruitProcessor(5, 0);
+// console.log(fruitProcessor(5, 0));
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(num);
