ba34471efe6a44de12c321aa5d8c77425675d18d
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-04-25 14:11:43 +0300

    Lecture 62: Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+// Lecture 59: Using Google, StackOverflow and MDN
+
 /*
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
