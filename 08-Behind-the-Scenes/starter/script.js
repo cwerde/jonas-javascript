792f8dd19dce766531395795731c7a15fd88573a
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-05-09 23:32:28 +0300

    95. Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Lecture 93: Scoping in Practice
+
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   // console.log(firstName);
@@ -12,10 +15,10 @@ function calcAge(birthYear) {
       var millenial = true;
 
       // Creating NEW variable with same name as outer scope's variable
-      const firstName = "Steven";
+      const firstName = 'Steven';
 
       // Reassigning outer scope's variable
-      output = "NEW OUTPUT!";
+      output = 'NEW OUTPUT!';
 
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
@@ -23,7 +26,6 @@ function calcAge(birthYear) {
       function add(a, b) {
         return a + b;
       }
-
     }
 
     // console.log(str);
@@ -37,7 +39,58 @@ function calcAge(birthYear) {
   return age;
 }
 
-const firstName = "Jonas";
+const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+// Lecture 95: Hoisting and TDZ in Practice
+
+/*
+// Variables
+// console.log(me); // undefined
+// console.log(job); // Uncaught ReferenceError
+// console.log(year); // Uncaught ReferenceError
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+// console.log(addDecl(2, 3)); // 5
+// console.log(addExpr(2, 3)); // Uncaught ReferenceError
+// console.log(addArrow(2, 3)); // Uncaught ReferenceError
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
+*/
