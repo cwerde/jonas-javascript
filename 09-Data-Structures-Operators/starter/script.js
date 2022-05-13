df394688ed64cf5d6be07e7a06ba4fd13d3c9810
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-05-13 17:42:48 +0300

    106. Rest Pattern and Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,10 +12,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -30,6 +26,28 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`);
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
 };
 
 ////////////////////////////////////////////////////////////
@@ -38,6 +56,7 @@ const restaurant = {
 
 // Lecture 103: Destructuring Arrays
 
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -57,7 +76,6 @@ console.log(arr);
 // console.log(main, secondary);
 
 // Switching variables
-
 // without destructuring
 // const temp = main;
 // main = secondary;
@@ -89,3 +107,181 @@ console.log(i, j, k);
 
 const [p = 1, q = 1, r = 1] = [8];
 console.log(p, q, r); // 8 1 1
+*/
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+// Lecture 104: Destructuring Objects
+
+/*
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+})
+
+restaurant.orderDelivery({
+  address: "Via del Sole, 21",
+  starterIndex: 1,
+})
+
+const {
+  name,
+  openingHours,
+  categories
+} = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const {
+  menu = [], starterMenu: starters = []
+} = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+
+({
+  a,
+  b
+} = obj);
+console.log(a, b);
+
+// Nested objects
+// const { fri } = restaurant.openingHours;
+// console.log(fri);
+
+// const { fri: { open, close } } = openingHours;
+// console.log(open, close);
+
+const {
+  fri: {
+    open: o,
+    close: c
+  }
+} = openingHours;
+console.log(o, c);
+*/
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+// Lecture 105: The Spread Operator (...)
+
+/*
+const arr = [7, 8, 9];
+console.log(arr);
+
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const goodNewArr = [1, 2, ...arr];
+console.log(goodNewArr);
+console.log(...goodNewArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = "Jonas";
+const letters = [...str, " ", "S."];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`);
+
+// Real-world example!
+// const ingredients = [prompt("Let's make pasta! Ingredient 1: "), prompt("Ingredient 2: "), prompt("Ingredient 3: ")];
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = {
+  foundedIn: 1998,
+  ...restaurant,
+  founder: "Guiseppe"
+}
+console.log(newRestaurant);
+
+const restaurantCopy = {
+  ...restaurant
+};
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+*/
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+// Lecture 106: Rest Pattern and Parameters
+
+/*
+// 1) Destructuring
+// SPREAD, because on RIGHT side of = 
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of = 
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// Rest collects all the array elements after the last variable
+// Rest is must be used at the end of the destruction
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const {
+  sat,
+  ...weekdays
+} = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  console.log(numbers);
+
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
+*/
