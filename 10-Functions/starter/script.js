b20ead519d661a5916dc3d24ffb243a382cfe199
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-06-08 16:52:11 +0300

    136. Immediately Invoked Function Expressions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -281,3 +281,31 @@ const addVATPar = addTaxPar(23);
 console.log(addVATPar(100));
 console.log(addVATPar(23));
 */
+
+////////////////////////////////////////////////////////////
+// Lecture 136: Immediately Invoked Function Expressions (IIFE)
+
+/*
+const runOnce = function () {
+  console.log('This will never run again.');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again.');
+  // const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again.'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
