12c33385a91ddb4921838c3885fdd47a871dd790
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-07-21 17:39:09 +0300

    199. Lazy Loading Images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -132,6 +132,55 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// // // // //
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+const revealObserver = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealObserver, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
+
+// // // // //
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  entry.target.src = entry.target.dataset.src;
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 //////////////////////////////////////////////////
 // Lecture 186: Selecting, Creating, and Deleting Elements
 
@@ -540,4 +589,66 @@ headerObserver.observe(header);
 */
 
 //////////////////////////////////////////////////
-// Lecture 198:  Revealing Elements on Scroll
+// Lecture 198: Revealing Elements on Scroll
+
+// Reveal sections
+/*
+const allSections = document.querySelectorAll('.section');
+const revealObserver = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealObserver, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
+*/
+
+//////////////////////////////////////////////////
+// Lecture 199: Lazy Loading Images
+
+// Lazy loading images
+/*
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  
+  // entry.target.classList.remove('lazy-img');
+  // const source = entry.target.getAttribute('data-src');
+  // entry.target.setAttribute('src', source);
+  
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  entry.target.src = entry.target.dataset.src;
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+*/
+
+//////////////////////////////////////////////////
+// Lecture 200: Building a Slider Component: Part 1
