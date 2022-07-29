b05f5deb94f8d13f310a4634f149fe987e9cf94f
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-07-29 17:51:06 +0300

    211. Prototypal Inheritance on Built-In Objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -86,3 +86,42 @@ console.log(jonas.hasOwnProperty('species'));
 
 ////////////////////////////////////////////////////////////
 // 211. Prototypal Inheritance on Built-In Objects
+/*
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+console.log(jonas.__proto__);
+// Object.prototype (top of the prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+console.dir(x => x + 1);
+*/
