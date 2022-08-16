8e737a3d0d234d061960762da2e413df6f49b7cf
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-08-16 17:26:29 +0300

    248. Our First AJAX Call: XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,8 +3,9 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+////////////////////////////////////////////////////////////
 
+// 248. Our First AJAX Call: XMLHttpRequest
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -14,19 +15,22 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
+    const language = Object.values(data.languages)[0];
+    const currency = Object.values(...Object.values(data.currencies))[0];
+
     // prettier-ignore
     const html = `
-  <article class="country">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages}</p>
-      <p class="country__row"><span>💰</span>${data.currencies}</p>
-    </div>
-  </article>
-  `;
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+    `;
 
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
@@ -36,3 +40,7 @@ const getCountryData = function (country) {
 getCountryData('turkey');
 getCountryData('portugal');
 getCountryData('germany');
+
+////////////////////////////////////////////////////////////
+
+// 250. Welcome to Callback Hell
