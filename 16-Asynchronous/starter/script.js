c92910f5f3ab408527ba57c32e628ad3c723c261
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-08-17 18:00:08 +0300

    253. Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,6 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ////////////////////////////////////////////////////////////
 
 // 248. Our First AJAX Call: XMLHttpRequest
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -40,7 +41,212 @@ const getCountryData = function (country) {
 getCountryData('turkey');
 getCountryData('portugal');
 getCountryData('germany');
+*/
 
 ////////////////////////////////////////////////////////////
 
 // 250. Welcome to Callback Hell
+
+/*
+const renderCountry = function (data, className = '') {
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(...Object.values(data.currencies))[0];
+
+  // prettier-ignore
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const requestNeighbour = new XMLHttpRequest();
+    requestNeighbour.open(
+      'GET',
+      `https://restcountries.com/v3.1/alpha/${neighbour}`
+    );
+    requestNeighbour.send();
+
+    requestNeighbour.addEventListener('load', function () {
+      const [dataNeighbour] = JSON.parse(this.responseText);
+      console.log(dataNeighbour);
+
+      // Render country 2
+      renderCountry(dataNeighbour, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('turkey');
+
+setTimeout(() => {
+  console.log('1 second past');
+  setTimeout(() => {
+    console.log('2 seconds past');
+    setTimeout(() => {
+      console.log('3 seconds past');
+      setTimeout(() => {
+        console.log('4 seconds past');
+        setTimeout(() => {
+          console.log('5 seconds past');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+////////////////////////////////////////////////////////////
+
+// 251. Promises and the Fetch API
+/*
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+request.send();
+*/
+
+/*
+const request = fetch('https://restcountries.com/v3.1/name/turkey');
+console.log(request);
+*/
+
+////////////////////////////////////////////////////////////
+
+// 252. Consuming Promises
+
+/*
+const renderCountry = function (data, className = '') {
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(...Object.values(data.currencies))[0];
+
+  // prettier-ignore
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+*/
+
+/*
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(...data);
+      renderCountry(...data);
+    });
+};
+*/
+
+/*
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('turkey');
+*/
+
+////////////////////////////////////////////////////////////
+
+// 253. Chaining Promises
+
+/*
+const renderCountry = function (data, className = '') {
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(...Object.values(data.currencies))[0];
+
+  // prettier-ignore
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      // Country 3
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+getCountryData('portugal');
+*/
+
+////////////////////////////////////////////////////////////
+
+// 254. Handling Rejected Promises
