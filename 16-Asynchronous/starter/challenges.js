3571a80948e1b5f2d92ab56f939bdcea8e205b2b
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-08-20 10:57:39 +0300

    261. Coding Challenge #2

--- 16-Asynchronous/starter/challenges.js
+++ 16-Asynchronous/starter/challenges.js
@@ -131,9 +131,67 @@ Images in the img folder. Test the error handler by passing a wrong image path. 
 
 // GOOD LUCK 😀
 
+/*
+const imgContainer = document.querySelector('.images');
+let currentImg;
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
+};
+
+createImage(`img/img-1.jpg`)
+  .then(function (img) {
+    currentImg = img;
+    console.log(`Image 1 loaded.`);
+    return wait(2);
+  })
+  .then(function () {
+    currentImg.style.display = 'none';
+
+    return createImage(`img/img-2.jpg`);
+  })
+  .then(function (img) {
+    currentImg = img;
+    console.log(`Image 2 loaded.`);
+    return wait(2);
+  })
+  .then(function () {
+    currentImg.style.display = 'none';
+
+    return createImage(`img/img-3.jpg`);
+  })
+  .then(function (img) {
+    currentImg = img;
+    console.log(`Image 3 loaded.`);
+    return wait(2);
+  })
+  .then(function () {
+    currentImg.style.display = 'none';
+  })
+  .catch(error => console.error(error));
+*/
+
 ////////////////////////////////////////////////////////////
 
-// 267. Coding Challenge #2
+// 267. Coding Challenge #3
 
 // Your tasks:
 
