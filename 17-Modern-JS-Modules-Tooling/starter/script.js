225a11525d104b935fc771c2e97f5980724b4ae4
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-08-26 15:43:14 +0300

    275. CommonJS Modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,117 @@
+// 272. Exporting and Importing in ES6 Modules
+
+// Importing module
+
+/*
+import './shoppingCart.js';
+console.log('Importing module');
+console.log(shippingCost);
+*/
+
+/*
+import { addToCart, totalPrice as price, quantity } from './shoppingCart.js';
+addToCart('bread', 5);
+console.log(price, quantity);
+*/
+
+/*
+import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
+*/
+
+/*
+// import add, { addToCart, totalPrice as price, quantity } from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+console.log(cart);
+*/
+
+////////////////////////////////////////////////////////////
+// 273. Top-Level await (ES2022)
+
+/*
+console.log('Start fetching...');
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+console.log(res);
+const data = await res.json();
+console.log(data);
+console.log('Something else.');
+*/
+
+/*
+import './shoppingCart.js';
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  return { type: data.at(-1).title, body: data.at(-1).body };
+};
+*/
+
+// Not very clean
+/*
+const lastPost = getLastPost();
+console.log(lastPost);
+lastPost.then(last => console.log(last));
+*/
+
+/*
+const lastPost = await getLastPost();
+console.log(lastPost);
+*/
+
+////////////////////////////////////////////////////////////
+// 274. The Module Pattern
+
+/*
+const ShoppingCart = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost}).`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier.`);
+  };
+
+  return { addToCart, cart, totalPrice, totalQuantity };
+})();
+
+ShoppingCart.addToCart('apples', 4);
+ShoppingCart.addToCart('pizza', 2);
+console.log(ShoppingCart);
+console.log(ShoppingCart.shippingCost);
+*/
+
+////////////////////////////////////////////////////////////
+// 275. CommonJS Modules
+
+// Export
+/*
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (shipping cost is ${shippingCost}).`
+  );
+};
+*/
+
+// Import
+/*
+const {addToCart} = require("./shoppingCart.js")
+*/
+
+////////////////////////////////////////////////////////////
+// 276. A Brief Introduction to the Command Line
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,36 @@
+// 272. Exporting and Importing in ES6 Modules
+
+// Exporting module
+
+/*
+console.log('Exporting module');
+const shippingCost = 10;
+*/
+
+/*
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart.`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+export { totalPrice, totalQuantity as quantity };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart.`);
+}
+*/
+
+////////////////////////////////////////////////////////////
+// 273. Top-Level await (ES2022)
+
+// Blocking code
+/*
+console.log('Start fetching users...');
+await fetch(`https://jsonplaceholder.typicode.com/users`);
+console.log('Finish fetching users...');
+*/
