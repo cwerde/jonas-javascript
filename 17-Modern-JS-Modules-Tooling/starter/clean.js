9503bdc616a5da9f0832068a6f623d38ac71b014
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-09-01 15:54:16 +0300

    281. Let's Fix Some Bad Code: Part 1

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,3 +1,4 @@
+/*
 var budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
@@ -62,3 +63,109 @@ var bigExpenses = function (limit) {
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+*/
+
+////////////////////////////////////////////////////////////
+// 281. Let's Fix Some Bad Code: Part 1
+/*
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
+  user = user.toLowerCase();
+
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+  // const limit = spendingLimits?.[user] ?? 0;
+  // const limit = getLimit(user);
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpenses = function () {
+  budget.forEach(function (entry) {
+    // const limit = spendingLimits?.[entry.user] ?? 0;
+    // const limit = getLimit(entry.user);
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+  });
+};
+checkExpenses();
+
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entry of budget) 
+    output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : "";
+  output = output.slice(0, -2); // Remove last '/'
+
+  console.log(output);
+};
+console.log(budget);
+logBigExpenses(100);
+*/
+
+////////////////////////////////////////////////////////////
+// 283. Let's Fix Some Bad Code: Part 2
+
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
+  user = user.toLowerCase();
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpenses = function () {
+  budget.forEach(function (entry) {
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+  });
+};
+checkExpenses();
+
+const logBigExpenses = function (bigLimit) {
+  const output = budget
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  console.log(output);
+};
+console.log(budget);
+logBigExpenses(100);
