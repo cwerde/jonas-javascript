3bda5980b8368398587b18cd44d757fad9b3ea34
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-09-06 17:29:26 +0300

    293. Helpers and Configuration Files

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -5,14 +5,6 @@ import recipeView from './views/recipeView.js';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -30,7 +22,7 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    console.error(error);
+    console.error(`💥 ${error} 💥`);
   }
 };
 
