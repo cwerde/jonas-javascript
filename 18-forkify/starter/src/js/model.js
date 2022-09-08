d77d0ef8872cd0c6d0f43b1984f72e1e482df4c0
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-09-08 17:21:18 +0300

    298. Implementing Pagination - Part 1

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,5 +1,4 @@
-import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -7,6 +6,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -50,4 +51,13 @@ export const loadSearchResults = async function (query) {
   } catch (error) {
     throw error;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0 is what we want
+  const end = page * state.search.resultsPerPage; // 9 is what we want
+
+  return state.search.results.slice(start, end);
 };
