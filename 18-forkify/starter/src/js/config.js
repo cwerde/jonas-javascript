f21716feeb013f3d71f4692b4a8719a72d3362ec
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-09-14 18:01:38 +0300

    309. Uploading a New Recipe - Part 3

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '73989101-d414-4a8c-8c97-246c0339a7ea';
+export const MODAL_CLOSE_SEC = 2.5;
