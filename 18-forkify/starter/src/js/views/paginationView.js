2b10ce46fab52719226fece6832fea4648d9516f
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2022-09-15 17:17:02 +0300

    313. Simple Deployment with Netlify

--- 18-forkify/starter/src/js/views/paginationView.js
+++ 18-forkify/starter/src/js/views/paginationView.js
@@ -1,75 +1,22 @@
-import icons from 'url:../../img/icons.svg';
-import View from './View.js';
+class SearchView {
+  #parentElement = document.querySelector('.search');
 
-class PaginationView extends View {
-  _parentElement = document.querySelector('.pagination');
-
-  addHandlerClick(handler) {
-    this._parentElement.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const btn = e.target.closest('.btn--inline');
-      if (!btn) return;
-
-      const goToPage = +btn.dataset.goto;
-      handler(goToPage);
-    });
+  getQuery() {
+    const query = this.#parentElement.querySelector('.search__field').value;
+    this.#clearInput();
+    return query;
   }
 
-  _generateMarkup() {
-    const curPage = this._data.page;
-    const numPages = Math.ceil(
-      this._data.results.length / this._data.resultsPerPage
-    );
+  #clearInput() {
+    this.#parentElement.querySelector('.search__field').value = '';
+  }
 
-    // Page 1, and there are other pages
-    if (curPage === 1 && numPages > 1) {
-      // prettier-ignore
-      return `
-        <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-      `;
-    }
-
-    // Last page
-    if (curPage === numPages && numPages > 1) {
-      // prettier-ignore
-      return `
-        <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-      `;
-    }
-
-    // Other page
-    if (curPage < numPages) {
-      // prettier-ignore
-      return `
-      <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-left"></use>
-        </svg>
-        <span>Page ${curPage - 1}</span>
-      </button>
-      <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
-        <span>Page ${curPage + 1}</span>
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-right"></use>
-        </svg>
-      </button>
-      `;
-    }
-
-    // Page 1, and there are NO other pages
-    return '';
+  addHandlerSearch(handler) {
+    this.#parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
   }
 }
 
-export default new PaginationView();
+export default new SearchView();
